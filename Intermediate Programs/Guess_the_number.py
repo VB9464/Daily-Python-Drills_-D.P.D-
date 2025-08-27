f35a9497acb20cb5170bb12f2c01a66cd3a39177
Vishal Bagal <bagal9464@gmail.com>
2025-08-27 08:29:28 +0530

    Update and rename .py to Guess_the_number.py

---
+++ Intermediate Programs/Guess_the_number.py
@@ -0,0 +1,32 @@
+import random
+
+def guess_the_number():
+   print("🎯 Welcome to Guess the Number Game!")
+   print("Enter Number Between 1 to 100")
+
+   # computer chooses the random number between 1 to 100
+   number_to_guess = random.randint(1,100)
+   attempts = 0
+   while True :
+       try:
+           # Take input from user
+           guess = int(input("Enter you guessed Number"))
+           attempts = 0
+
+           if guess < number_to_guess:
+                print(" Too low! Try again.")
+           elif guess > number_to_guess:
+                print("Too high! Try again.")
+           else: 
+                print(f"Congratulations! You guessed it in {attempts} attempts ")
+                break
+       except ValueError:
+           print(" Please enter a valid Number!")
+
+
+guess_the_number()
+# run the game 
+#if __name__ == "__main__":
+#  guess_the_number()
+
+   
