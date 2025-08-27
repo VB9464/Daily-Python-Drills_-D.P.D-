1975589b0e3a95819632ff73caa2539d7b3bb78b
Vishal Bagal <bagal9464@gmail.com>
2025-08-27 07:51:03 +0530

    Create  Hello_world_input_output.py

---
+++ Beginner Programs/Hello_world_input_output.py
@@ -0,0 +1,13 @@
+print('Hello World')
+text = "Hello World"
+print(text)
+
+
+Name = input("Enter your Name")
+print("Hello",Name)
+print(f"Hello,{Name}")
+
+def hello():
+    print("Hello World")
+    print(f"hello,{Name}")
+hello()
