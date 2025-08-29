08c8dae23805f50cb43a2345d222b15065493ea9
Vishal Bagal <bagal9464@gmail.com>
2025-08-29 06:29:04 +0530

    Add files via upload

---
+++ Python & DSA/Hello_world_input_output.py
@@ -0,0 +1,14 @@
+print('Hello World')
+text = "Hello World"
+print(text)
+
+
+Name = input("Enter your Name")
+print("Hello",Name)
+print(f"Hello,{Name}")
+
+
+def hello():
+    print("Hello World")
+    print(f"Hello,{Name}")
+hello()
