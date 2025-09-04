1db05a88306fb723122913c54b92b590e5b04cb8
Vishal Bagal <bagal9464@gmail.com>
2025-09-04 21:47:03 +0530

    Update Mini To-Do list.py

--- Python & DSA/Mini To-Do list.py
+++ Python & DSA/Mini To-Do list.py
@@ -1,1 +1,24 @@
+tasks = []
 
+while True:
+  action = input("Add task (a), Show tasks (s), Exit (e):").lower()
+
+  if action =="a":
+    task = input("Enter task:")
+    tasks.append(task)
+    print(f"Task added: {task}")
+
+  elif action =="s":
+    if not tasks:
+      print("No tasks yet.")
+    else:
+      print("\n Your Task: ")
+      for i,t in enumerate(tasks,1):
+        print(f"{i}. {t}")
+
+  elif action == "e":
+    print(" Goodbye!")
+    break
+
+  else:
+    print(" Invalid choice.Try again.")
