3fa4a1378363ef9505439595b2834e45c90caeae
Vishal Bagal <bagal9464@gmail.com>
2025-09-15 21:59:47 +0530

    Update 01_Stack.py

--- Python & DSA/01_Stack.py
+++ Python & DSA/01_Stack.py
@@ -3,3 +3,18 @@
 # Pop  ---> remove item
 # Peek ---> see top item
 
+# Stack implementation using Python list
+stack = [10,20,30] #empty stack
+
+# Push elements
+stack.append(50) # push 10
+stack.append(60)
+stack.append(70)
+print("Stack after pushes:",stack)
+
+#Pop element
+print("Popped element:", stack.pop()) # removes 30
+print("Stack after pop:", stack)
+
+# Peek element (top of stack)
+print("Top element:",stack[-1]) #last element
